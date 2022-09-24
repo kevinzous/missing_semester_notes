fe7063b0cbe11aa3bacea177c500b47dfcd8b296
kevinzhou <kevin75ha@hotmail.fr>
2022-09-24 11:32:12 +0200

    apply black

--- 07_files/07sorts.py
+++ 07_files/07sorts.py
@@ -7,16 +7,17 @@ def test_sorted(fn, iters=1000):
         assert fn(l) == sorted(l)
         # print(fn.__name__, fn(l))
 
+
 @profile
 def insertionsort(array):
 
     for i in range(len(array)):
-        j = i-1
+        j = i - 1
         v = array[i]
         while j >= 0 and v < array[j]:
-            array[j+1] = array[j]
+            array[j + 1] = array[j]
             j -= 1
-        array[j+1] = v
+        array[j + 1] = v
     return array
 
 
@@ -33,22 +34,22 @@ def quicksort_inplace(array, low=0, high=None):
     if len(array) <= 1:
         return array
     if high is None:
-        high = len(array)-1
+        high = len(array) - 1
     if low >= high:
         return array
 
     pivot = array[high]
-    j = low-1
+    j = low - 1
     for i in range(low, high):
         if array[i] <= pivot:
             j += 1
             array[i], array[j] = array[j], array[i]
-    array[high], array[j+1] = array[j+1], array[high]
+    array[high], array[j + 1] = array[j + 1], array[high]
     quicksort_inplace(array, low, j)
-    quicksort_inplace(array, j+2, high)
+    quicksort_inplace(array, j + 2, high)
     return array
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     for fn in [quicksort, quicksort_inplace, insertionsort]:
         test_sorted(fn)
